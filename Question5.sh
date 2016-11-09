bb320c1b55566a61b935532df78e21bcb47f4ac4
Brookealynn <brookeresendes@outlook.com>
2016-11-08 23:00:23 -0600

    Updated using Bash

--- Question5.sh
+++ Question5.sh
@@ -2,7 +2,4 @@
 
 #This script looks for all executable files in the current directory and adds "#Developed in cisc220" just after the #!/bin/bash line
 
-#Checking for executable files
-find <dir> -executable  #or should we be using execvp to run a find command (p 29)
-
-#should this be in an if statement?
+find .-type f -executable $ awk '#!/bin/bash' echo '#Developed in cisc220' 
