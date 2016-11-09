49692c835bea11682b8a035f57d3500d005ca9e8
Brookealynn <brookeresendes@outlook.com>
2016-11-08 23:07:58 -0600

    Need to figure out awk command error

--- Question5.sh
+++ Question5.sh
@@ -2,4 +2,6 @@
 
 #This script looks for all executable files in the current directory and adds "#Developed in cisc220" just after the #!/bin/bash line
 
-find .-type f -executable $ awk '#!/bin/bash' echo '#Developed in cisc220' 
+#find command finds the executable files, and awk is supposed to search for bin/bash and place the sentence underneath it
+#not working atm
+find .-type f -executable $ awk '/#!/bin/bash/{print;print "#Developed in cisc220";next}1' 
