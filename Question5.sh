d6e05db6373f13c7f8d26d98b5ea38cc3b549908
Brookealynn <brookeresendes@outlook.com>
2016-11-09 15:06:28 -0600

    Working, but only when you manually input the file name. The pipe isnt doing its job for some reason?

--- Question5.sh
+++ Question5.sh
@@ -3,5 +3,6 @@
 #This script looks for all executable files in the current directory and adds "#Developed in cisc220" just after the #!/bin/bash line
 
 #find command finds the executable files, and awk is supposed to search for bin/bash and place the sentence underneath it
-#not working atm
-find .-type f -executable $ awk '/#!/bin/bash/{print;print "#Developed in cisc220";next}1' 
+find . -type f -executable | sed 's@#!/bin/bash@#!/bin/bash\n"#Developed in cisc220"@' filename
+
+
