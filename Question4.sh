0f2173f89fbd14c65536ef94585e88e86225d657
dseekatz <dseekatz@gmail.com>
2016-11-13 10:35:47 -0500

    Shadis assignment sheet had a typo.  Fixed lsblk command options. Also added grep.

--- Question4.sh
+++ Question4.sh
@@ -4,4 +4,4 @@
 #Then display only the two partitions with the largest sizes 
 #$df -H lists partition size but not ascendingly by size
 
-lsblk --noheadings -raw | sed s/"G"/" G"/ | sed s/"M"/" M"/ | sort -k2,2 -k1,1n
+lsblk --noheadings --raw | grep -e part | sed s/"G"/" G"/ | sed s/"M"/" M"/ | sort -k2,2 -k1,1n
