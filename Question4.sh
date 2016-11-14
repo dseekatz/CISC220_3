e6d1857632719780c0d4ecc337441249a9bb205f
Brooke <13brr2@queensu.ca>
2016-11-14 11:15:36 -0500

    Update Question4.sh
    Works!

--- Question4.sh
+++ Question4.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 #Write a single line command using pipes to sort the output of the lsblk-noheadings -raw command by partition size 
-#Then display only the two partitions with the largest sizes 
-#$df -H lists partition size but not ascendingly by size
-
-lsblk --noheadings --raw | grep -e part | sed s/"G"/" G"/ | sed s/"M"/" M"/ | sort -k2,2 -k1,1n
+#head command displays only the two partitions with the largest sizes 
+lsblk --noheadings --raw | grep -e part | sed s/"G"/" G"/ | sed s/"M"/" M"/ | sort -k4n | head -n2
