11ede6ef73ccd784a86e8d9b59a0bb30691c363b
Brooke <13brr2@queensu.ca>
2016-11-14 11:16:56 -0500

    Update Question5.sh

--- Question5.sh
+++ Question5.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #This script looks for all executable files in the current directory and adds "#Developed in cisc220" just after the #!/bin/bash line
-
 #find command finds the executable files, and awk is supposed to search for bin/bash and place the sentence underneath it
 find . -type f -executable | sed 's@#!/bin/bash@#!/bin/bash\n"#Developed in cisc220"@' *.sh
 
