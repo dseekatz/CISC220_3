e89fc5faf35c4d22fc4f7debd31b9e43744e833f
dseekatz <dseekatz@gmail.com>
2016-11-14 19:39:55 -0500

    Update Question4.sh

--- Question4.sh
+++ Question4.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# David Seekatz		16das4 20037710
+# Sean Eylott	    	13se18 10141657
+# Simon Ungar         	14seau 10149546
+# Brooke Resendes     	13brr2 10141112
 
 #Write a single line command using pipes to sort the output of the lsblk-noheadings -raw command by partition size 
 #The head command displays only the two partitions with the largest sizes 
